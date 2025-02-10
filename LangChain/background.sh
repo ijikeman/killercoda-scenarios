ad79c2276da044e4184b030ba1f5ec67678b205b
ijikeman <keiji.ue@gmail.com>
2025-02-10 16:50:42 +0900

    fixed

--- LangChain/background.sh
+++ LangChain/background.sh
@@ -14,10 +14,13 @@ python3 -m venv ~/venv
 source ~/venv/bin/activate
 
 # Install LangChain
+echo 'start Langchain install' >> $LOG
 pip install langchain langchain-community langchain-openai
 # openai qdrant-client sentence-transformers tiktoken
+echo 'end Langchain install' >> $LOG
 
 # Install Docker
+echo 'start docker install' >> $LOG
 apt-get install ca-certificates curl -y
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
@@ -28,9 +31,11 @@ echo \
   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+echo N|apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+echo 'end docker install' >> $LOG
 
-# qraud
+# qdraud
+echo 'start qdrant install' >> $LOG
 cat <<EOF > compose.yml
 services:
   qdrant:
@@ -42,6 +47,7 @@ services:
       - 6333:6333
 EOF
 docker compose up -d
+echo 'end qdrant install' >> $LOG
 
 # Finished
 echo "Background Done" >> ${LOG}
