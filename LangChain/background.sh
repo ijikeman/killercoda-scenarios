8d2f14a621b875ec9857bd557506021df728693b
ijikeman <keiji.ue@gmail.com>
2025-02-15 09:15:29 +0900

    fixed

--- LangChain/background.sh
+++ LangChain/background.sh
@@ -3,7 +3,7 @@
 LOG='/var/log/background.log'
 
 # main
-echo "Background Started" >> ${LOG}
+echo "[Log]: Background Started" >> ${LOG}
 apt-get update
 
 # Install venv
@@ -14,13 +14,13 @@ python3 -m venv ~/venv
 source ~/venv/bin/activate
 
 # Install LangChain
-echo 'start Langchain install' >> $LOG
+echo '[Log]: start Langchain install' >> $LOG
 pip install langchain langchain-community langchain-openai
 # openai qdrant-client sentence-transformers tiktoken
-echo 'end Langchain install' >> $LOG
+echo '[Log]: end Langchain install' >> $LOG
 
 # Install Docker
-echo 'start docker install' >> $LOG
+echo '[Log]: start docker install' >> $LOG
 apt-get install ca-certificates curl -y
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
@@ -32,10 +32,10 @@ echo \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
 echo N|apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-echo 'end docker install' >> $LOG
+echo '[Log]: end docker install' >> $LOG
 
 # qdraud
-echo 'start qdrant install' >> $LOG
+echo '[Log]: start qdrant install' >> $LOG
 cat <<EOF > compose.yml
 services:
   qdrant:
@@ -47,8 +47,8 @@ services:
       - 6333:6333
 EOF
 docker compose up -d
-echo 'end qdrant install' >> $LOG
+echo '[Log]: end qdrant install' >> $LOG
 
 # Finished
-echo "Background Done" >> ${LOG}
+echo "[Log]: Background Done" >> ${LOG}
 touch /tmp/background-finished
--- LangChain/foreground.sh
+++ LangChain/foreground.sh
@@ -1,2 +1,2 @@
-while [ ! -f /tmp/background-finished ]; do sleep 5; echo 'check file'; done
+while [ ! -f /tmp/background-finished ]; do sleep 5; tail -1 /var/log/background.log && echo 'check file'; done
 echo Hello and Welcom to this scenario!
